f9acee93abe3040af58bdf8489a25e960538060f
xionghengheng <345797400@qq.com>
2014-12-28 16:37:16 +0800

    字符分割（动态规划）

--- word break.cpp
+++ word break.cpp
@@ -3,6 +3,10 @@
 思路：用dp来记录以序号i为结尾的字符串能否在字典中匹配成功。
 当有字符s[i]匹配不成功，而该字符前面的dp[j]出现了true，那么可以看是否有分割字符，即该j+1到i的子串看是否可以在字典中匹配。
 例如s="abcd" dict=[a,b,abc,cd]
+dp[0]=true
+dp[1]=true
+dp[2]=true
+dp[3]=false，同时需要确定0-3,1-3，2-3之间是否能在dict中找到，若找到则dp[3]=true
 */
 class Solution {
 public:
